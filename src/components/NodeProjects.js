55637cd6e31f12274349b0d66cb5bc624a1e47d4
rocsteady888 <rocsteady888@gmail.com>
2019-04-02 22:03:55 -0400

    initial commit

---
+++ src/components/NodeProjects.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import ComplexCard from './common/ComplexCard';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function NodeProjects(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12} sm={6} md={4}>
+          <ComplexCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ComplexCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ComplexCard />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+NodeProjects.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NodeProjects);
