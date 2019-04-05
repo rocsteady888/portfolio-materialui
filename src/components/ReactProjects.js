4281f4d8b42b1aa0eb927451e01ef4f2578aad0a
rocsteady888 <rocsteady888@gmail.com>
2019-04-05 19:01:38 -0400

    removed unecessary cards

--- src/components/ReactProjects.js
+++ src/components/ReactProjects.js
@@ -35,15 +35,13 @@ class ReactProjects extends React.Component {
               headline={"Full stack application using the MERN stack. React-Redux, Express, Mongoose, MongoDB.  This app solves a common issue for many companies with unusual work hours or self employed people. People that work 24 hour shifts, overnight shifts, or self employed people have few options for keeping track of their work week."}
               paragraph1={"Each user is authenticated with a Passport JS strategy that leverages JWT web tokens as bearer tokens stored in local storage in the header."}
               paragraph2={"The backend of this application stores data in a mongoDB with mongoose as the ODM.  There are 2 models, the User model which stores user data and the Shift model which stores clock in time and description as well as clock out time and description.  The user has the ability to edit or delete their shifts."}
-              paragraph3={"The front end uses the React and Redux libraries which make is super quick and responsive.  Nested components in this application will easily be reused in further development.  The application is also styled with Material UI which helps make all the components familar to users."}
+              paragraph3={"The front end uses the React and Redux libraries which make is super quick and responsive.  Nested components in this application will easily be reused in further development.  The functionality of Redux is being replaced React Hooks in any new development.  The application is also styled with Material UI which helps make all the components familar to users."}
               paragraph4={"This application was designed to be used on mobile for clocking in and out on the go.  Then tables are available on desktop or tablet to make reviewing the time card easy."}
-          />
+            />
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <ComplexCard />
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <ComplexCard />
           </Grid>
         </Grid>
       </div>
