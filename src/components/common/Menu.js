6eb18eac76f0989d85ebdcdce1f39e2f2055779f
rocsteady888 <rocsteady888@gmail.com>
2019-04-07 20:00:22 -0400

    changed to a functional component with hooks

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -5,50 +5,46 @@ import MenuItem from '@material-ui/core/MenuItem';
 import resumePDF from '../../documents/Ryan O’Connor-pdf.pdf';
 import resumeWord from '../../documents/Ryan O’Connor-word.docx';
 
-class SimpleMenu extends React.Component {
-  state = {
-    anchorEl: null,
-  };
+function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  handleClick = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
-  render() {
-    const { anchorEl } = this.state;
-
-    return (
-      <div>
-        <Button
-          aria-owns={anchorEl ? 'simple-menu' : undefined}
-          aria-haspopup="true"
-          onClick={this.handleClick}
-        >
-          Resume
-        </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={this.handleClose}
-        >
-          <MenuItem onClick={this.handleClose}>
-            <Button fullWidth color="inherit" target="_blank" href={resumePDF}>Open New Tab</Button>
-          </MenuItem>
-          <MenuItem onClick={this.handleClose}>
-            <Button fullWidth color="inherit" href={resumePDF}>Download pdf</Button>
-          </MenuItem>
-          <MenuItem onClick={this.handleClose}>
-            <Button fullWidth color="inherit" href={resumeWord}>Download docx</Button>
-          </MenuItem>
-        </Menu>
-      </div>
-    );
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
   }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  return (
+    <div>
+      <Button
+        aria-owns={anchorEl ? 'simple-menu' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClick={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <Button fullWidth color="inherit" target="_blank" href={resumePDF}>Open New Tab</Button>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Button fullWidth color="inherit" href={resumePDF}>Download pdf</Button>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Button fullWidth color="inherit" href={resumeWord}>Download docx</Button>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
 }
 
 export default SimpleMenu;
+
+
